a59e191c6ed9c6382b0410b119b0302789da4fb9
James Forrest <jsforrest87@gmail.com>
2021-04-11 13:45:38 -0500

    Added Main Activity Layout and Main Menu Fragment

--- AcmeDepartmentStore/app/src/main/java/com/ads/app/MainActivity.java
+++ AcmeDepartmentStore/app/src/main/java/com/ads/app/MainActivity.java
@@ -2,13 +2,33 @@ package com.ads.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainMenuFragment menuFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragment(R.id.page_container, new MainMenuFragment(), MainMenuFragment.FRAGMENT_TAG);
+    }
+
+    private void addFragment(int containerId, Fragment fragment, String fragmentTag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(containerId, fragment, fragmentTag)
+                .disallowAddToBackStack()
+                .commit();
+    }
+
+    private void replaceFragment(int containerId, Fragment fragment, String fragmentTag, String backStackStateName) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerId, fragment, fragmentTag)
+                .addToBackStack(backStackStateName)
+                .commit();
     }
 }
---
+++ AcmeDepartmentStore/app/src/main/java/com/ads/app/MainMenuFragment.java
@@ -0,0 +1,35 @@
+package com.ads.app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainMenuFragment extends Fragment {
+
+    public static final String FRAGMENT_TAG = BuildConfig.APPLICATION_ID + ".MAIN_MENU_FRAGMENT_TAG";
+
+    public MainMenuFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainMenuFragment newInstance() {
+        MainMenuFragment fragment = new MainMenuFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
